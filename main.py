e0b3b3574559c205094df1d304204dbd5e3cf6a2
Kjagan1907 <Kjagan304@gmail.com>
2025-10-15 15:27:04 +0200

    adding the pixela graph

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ import requests
 import os
 from dotenv import load_dotenv
 
+USERNAME = 'jaganmohan'
+
 #create a new pixela user
-end_point = 'https://pixe.la/v1/users'
+user_end_point = 'https://pixe.la/v1/users'
 load_dotenv()  # Automatically reads .env in project folder
 load_dotenv(dotenv_path="token.env")  # <-- specify your file
-paramters = {
+create_user_paramters = {
     'token': os.getenv('MY_PIXELA_TOKEN'),
-    'username': 'jaganmohan',
+    'username': USERNAME,
     'agreeTermsOfService': 'yes',
     'notMinor': 'yes',    
 }
 
-response = requests.post(url=end_point, json=paramters)
+# creating a pixelation graph
+graph_end_point = f"{user_end_point}/{USERNAME}/graphs"
+
+graph_parameters = {
+    'id': 'graph1',
+    'name': 'My Habit Traker',
+    'unit': 'commit',
+    'type': 'int',
+    'color': 'ajisai',
+}
+
+headers = {
+    'X-USER-TOKEN': os.getenv('MY_PIXELA_TOKEN'),
+}
+
+response = requests.post(url=graph_end_point, json=graph_parameters, headers=headers)
 print(response.text)
